f45870ed867ed5e4cee84ebf0e5e779132fa167b
Mustafa Fares <mustaphafares445@gmail.com>
2026-06-25 00:36:18 +0300

    fix issue

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ContentRequest;
 use App\Http\Resources\ContentResource;
 use App\Models\Content;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileDoesNotExist;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\FileIsTooBig;
 
 class ContentController extends Controller
 {
@@ -29,18 +31,22 @@ class ContentController extends Controller
         return new ContentResource($content->load('media'));
     }
 
+    /**
+     * @throws FileIsTooBig
+     * @throws FileDoesNotExist
+     */
     public function update(ContentRequest $request, Content $content)
     {
         $content->update($request->validated());
 
         if ($request->hasFile('files')) {
-            $content->deleteCollection('files');
+            $content->deleteAllMedia();
             foreach ($request->file('files') as $file) {
                 $content->addMedia($file)->toMediaCollection('files');
             }
         }
 
-        return new ContentResource($content->load('media'))->response();
+        return ContentResource::make($content->load('media'))->response();
     }
 
     public function destroy(Content $content)
